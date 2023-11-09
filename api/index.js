45c5d15fac58113555a62f9b259665228f1bb161
DominicLHTruong <lhturong2003@gmai.com>
2023-11-09 21:33:22 +0700

    update

--- api/index.js
+++ api/index.js
@@ -28,6 +28,10 @@ mongoose
     console.log('Error connecting to MongoDB ', err)
   })
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
